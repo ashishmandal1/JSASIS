7cab2840b1ea198a1328853d6a761ca320867806
ashishmandal1 <122786249+ashishmandal1@users.noreply.github.com>
2024-08-24 07:38:13 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+
+//alert(4 + 4) //we are using node.js not browser 
+console.log(3+3);
+
+/* Some datatypes
+1.number  = 2 to power something
+2.string (set of character stored in a single unit)
+3. boolean (True or False)
+4.null (Standard value)
+5.Undefined (not written anything inside given semicolen(;) directly)
+6.Symbol(it gives uniquness)
+*/
+console.log("Ashish");
+
+console.log(typeof "Ashish");
+console.log(typeof undefined);
+console.log(typeof null);
+
+
