96302fcef6ff43db554836919ff5d20ade60a39b
ashishmandal1 <122786249+ashishmandal1@users.noreply.github.com>
2024-08-24 19:12:14 +0000

    operations and comparisions basics with code

---
+++ 01_basics/03_1operations.js
@@ -0,0 +1,17 @@
+console.log(2+2);
+
+let str1 = "Hello"
+let str2 = " Ashish"
+let str3 =  str1 + str2
+console.log(str3); //Result = Hello Ashish
+console.log("1" + 2); //Result = 12
+console.log("1" + "2"); //Result = 12
+console.log(1 + 2); //Result = 3
+console.log("1" + 2 + 3); //Result = 122
+console.log(1 + 2 + "3"); //Result =33
+
+
+
+
+
+
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,8 @@
+console.log(2>1); //Result = true
+console.log("2" > 1); //Result = true
+console.log(null > 0 ); //false
+console.log(null==0); //false
+console.log(null>=0); //true
+
+
+
