8a0378771b499008be1147fa8545b57a7a79b9b6
ashishmandal1 <122786249+ashishmandal1@users.noreply.github.com>
2024-08-25 09:25:57 +0000

    comparision and datatypes

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -3,6 +3,6 @@ console.log("2" > 1); //Result = true
 console.log(null > 0 ); //false
 console.log(null==0); //false
 console.log(null>=0); //true
-
+ 
 
 
---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,13 @@
+/*primitive datatype
+1.string
+2.Number
+3.Boolean
+4.Null
+5.Undefined
+6.Symbol
+7.BigInt
+
+Non-Primitive datatype
+1.Array
+2.Objects
+3.Functions */
