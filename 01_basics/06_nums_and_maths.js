d9c6f6e6b063f5dbc1d8fe1fff6c80e4fe491579
ashishmandal1 <122786249+ashishmandal1@users.noreply.github.com>
2024-08-26 07:12:48 +0000

    maths and nums

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,18 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+console.log();
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));
+console.log(Math.floor(4.6));
+console.log(Math.random());
+
+
+
+
+
+
