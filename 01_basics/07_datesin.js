48fb3488ebf0730532e70e3ae058dd287b5d4b24
ashishmandal1 <122786249+ashishmandal1@users.noreply.github.com>
2024-08-26 08:30:24 +0000

    dates in JS

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,12 @@
+//Date
+let myDate = new Date()
+console.log(myDate);
+let myCreatedDate = new Date(2024, 8, 8)
+console.log(myCreatedDate.toDateString);
+
+let myTimeStamp =  Date.now()
+console.log(myTimeStamp);
+console.log(Date.now());
+
+
+
